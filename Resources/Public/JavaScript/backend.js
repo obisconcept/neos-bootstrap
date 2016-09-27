e7456ec4eb349fb4224bac395a86fe32371b9612
Dennis Irmscher <d.irmscher@obis-concept.de>
2016-09-27 15:58:53 +0200

    Removed special backend bootstrap js files

--- Resources/Public/JavaScript/backend.js
+++ Resources/Public/JavaScript/backend.js
@@ -2,19 +2,11 @@ var bootstrapHalfCss = '';
 var bootstrapFullCss = '';
 var bootstrapCss = '';
 
-var bootstrapHalfJs = '';
-var bootstrapFullJs = '';
-var bootstrapJs = '';
-
 $(document).ready(function () {
 
     bootstrapHalfCss = $('#bootstrap-css').attr('href').replace('main', 'main-backend-half');
     bootstrapFullCss = $('#bootstrap-css').attr('href').replace('main', 'main-backend-full');
     bootstrapCss = $('#bootstrap-css').attr('href');
-
-    bootstrapHalfJs = $('#bootstrap-js').attr('src').replace('bootstrap', 'bootstrap-backend-half');
-    bootstrapFullJs = $('#bootstrap-js').attr('src').replace('bootstrap', 'bootstrap-backend-full');
-    bootstrapJs = $('#bootstrap-js').attr('src');
 
     $('body').on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function(event) {
 
@@ -36,22 +28,18 @@ var changeBootstrap = function() {
     if (left > 0 && right == 0) {
 
         $('#bootstrap-css').attr('href', bootstrapHalfCss);
-        $('#bootstrap-js').attr('href', bootstrapHalfJs);
 
     } else if (right > 0 && left == 0) {
 
         $('#bootstrap-css').attr('href', bootstrapHalfCss);
-        $('#bootstrap-js').attr('href', bootstrapHalfJs);
 
     } else if (left > 0 && right > 0) {
 
         $('#bootstrap-css').attr('href', bootstrapFullCss);
-        $('#bootstrap-js').attr('href', bootstrapFullJs);
 
     } else if (right == 0 && left == 0) {
 
         $('#bootstrap-css').attr('href', bootstrapCss);
-        $('#bootstrap-js').attr('href', bootstrapJs);
 
     }
 
